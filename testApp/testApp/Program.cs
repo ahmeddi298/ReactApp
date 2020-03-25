a9cb24b248cb3ae84f1c5934990369136edd4398
Ahmed.Werghi.29 <ahmedwerghi27@gmail.com>
2020-03-25 03:01:04 +0100

    expected faultii

--- testApp/testApp/Program.cs
+++ testApp/testApp/Program.cs
@@ -10,7 +10,7 @@ namespace testApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world !!");
+            Console.WriteLine("hello world !!")
         }
     }
 }
